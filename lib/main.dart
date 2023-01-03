0b3cee9f6235ed317aeb267923142e59dc5bd129
Jayakrishnan-manoj <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2023-01-03 16:18:10 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:clima_app/screens/loading_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
